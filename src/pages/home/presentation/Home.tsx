649e1ec71b457f85310c7bcd5a6cfbfd014d651b
amannegi <akuro787898@gmail.com>
2024-10-19 02:30:28 +0530

    bug: remove react-icons import

--- src/pages/home/presentation/Home.tsx
+++ src/pages/home/presentation/Home.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { IconType } from "react-icons";
 import { FaEye, FaGithub, FaInfoCircle } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
